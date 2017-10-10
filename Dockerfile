a4b0f56f4d4aaac4cc970cefe90b2f77103aa6b1
53rb3rn4r <serbernar1@gmail.com>
2017-10-10 19:39:57 +0300

    Add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,46 @@
+FROM alpine:3.6
+MAINTAINER Sergey Berebko <serbernar1@gmail.com>
+
+
+RUN addgroup -S unprivileged -g 1000 && adduser -S unprivileged -u 1000
+
+RUN apk update
+RUN apk add --no-cache --update python py-pip \
+	&& apk add --no-cache --update --virtual .build-deps\
+		alpine-sdk \
+		make \
+		g++ \
+		musl-dev \
+		autoconf \
+		automake \
+		libtool \
+		libffi-dev \
+		gmp-dev \
+		python-dev \
+		openssl-dev \
+		git
+
+RUN apk add --no-cache --virtual=build-dependencies \
+	--repository http://nl.alpinelinux.org/alpine/edge/testing \
+	leveldb leveldb-dev
+
+RUN pip install jsonrpclib, plyvel, 'irc >= 11, <=14.0'
+
+RUN wget -O - "http://foundry.electrum.org/leveldb-dump/electrum-fulltree-100-latest.tar.gz" | tar --extract --gunzip --strip-components 1 --directory $path --file -
+
+
+COPY . app/
+WORKDIR /app
+RUN chmod +x run_electrum_server.py
+USER unprivileged
+
+EXPOSE 50001
+EXPOSE 8081
+EXPOSE 50002
+EXPOSE 8082
+
+
+# NOTICE: add database path to volume tag or another
+VOLUME /app
+
+CMD ["python", "run_electrum_server.py"]
--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="electrum-dash-server",
+    name="electrum-sibcoin-server",
     version="1.0",
-    scripts=['run_electrum_dash_server','electrum-dash-server'],
-    install_requires=['plyvel','jsonrpclib', 'irc >= 11, <=14.0', 'x11_hash'],
+    scripts=['run_electrum_sibcoin_server','electrum-sibcoin-server'],
+    install_requires=['plyvel', 'jsonrpclib', 'irc >= 11, <=14.0', 'x11_hash'],
     package_dir={
-        'electrumserver':'src'
+        'electrumserver': 'src'
         },
     py_modules=[
         'electrumserver.__init__',
@@ -21,10 +21,10 @@ setup(
         'electrumserver.ircthread',
         'electrumserver.stratum_tcp'
     ],
-    description="Dash Electrum Server",
+    description="Sibcon Electrum Server",
     author="Thomas Voegtlin ,ELM4ever, Propulsion, TheLazieR",
     author_email="thomasv@electrum.org, thelazier@gmail.com",
     license="MIT Licence",
     url="https://github.com/spesmilo/electrum-server/ , https://github.com/thelazier/electrum-dash-server/",
-    long_description="""Server for the Electrum Lightweight Dash Wallet"""
+    long_description="""Server for the Electrum Lightweight Sibcoin Wallet"""
 )
